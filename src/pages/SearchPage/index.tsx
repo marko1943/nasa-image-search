44a6b8402ef5d8bb4136279ac67c1ea8fea160fe
Marko <markostojiljkovic1943@gmail.com>
2023-07-27 18:19:37 +0200

    basic api call working

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,11 +1,24 @@
-import ShowPage from '@pages/ShowPage';
 import styles from './SearchPage.module.scss';
+import { useEffect, useState } from 'react';
+import { getImages } from '@/services/getImages';
 
 const SearchPage = () => {
+  const [, setImages] = useState();
+
+  useEffect(() => {
+    getImages('moon')
+      .then((res) => {
+        console.log(res);
+        setImages(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className={styles.SearchPage}>
       <h1>Hello, I'm a search page.</h1>
-      <ShowPage />
     </div>
   );
 };
--- src/services/getImages.ts
+++ src/services/getImages.ts
@@ -1,7 +1,9 @@
 import { API_URL } from '@/constants/API';
 
-export async function getBeers(size: number) {
-  const response = await fetch(API_URL + `?size=${size}`);
+export async function getImages(query: string, size: number = 10) {
+  const response = await fetch(
+    API_URL + 'search' + `?q=${query}` + `&page_size=${size}`
+  );
   if (!response.ok) {
     const message = `An error has occured: ${response.status}`;
     throw new Error(message);
