c169d70b1b73e86f02c229585fc10c34449f9af1
Marko <markostojiljkovic1943@gmail.com>
2023-08-04 11:37:59 +0200

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SearchPage from "@pages/SearchPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import classes from "./app.module.scss";
+import classes from "./App.module.scss";
 import ShowPage from "./pages/ShowPage";
 
 const router = createBrowserRouter([
